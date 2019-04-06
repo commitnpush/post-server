0916141d9ff12731057b795f0a100fa834c0965d
flynn <lognamu@gmail.com>
2019-04-06 20:46:25 +0900

    빌드설정 변경

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 
 app.use("/", express.static(path.join(__dirname, "../")));
-app.use("/post", express.static(path.join(__dirname, "../../post/dist/")));
 
 /* MONGODB CONNECTION */
 const db = mongoose.connection;
